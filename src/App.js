cbf33f9a3e8f7e3e0f63f89a03ec05557cdca069
jacagaca <jacagaca1@o2.pl>
2019-11-09 14:27:34 +0100

    pics + gameModule

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./styles.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Header>aaa</Header>
+      <Header></Header>
 
       <Main></Main>
     </div>
--- src/components/GameModule.js
+++ src/components/GameModule.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class GameModule extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <h1>Game</h1>
-      </div>
-    );
-  }
+function GameModule() {
+  let [count, setCount] = React.useState(5);
+  let i_id;
+
+  React.useEffect(() => {
+    i_id = setInterval(() => {
+      setCount(currCount => currCount - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(i_id);
+    };
+  }, []);
+
+  return (
+    <div className="GameModule">
+      <h1>Game</h1>
+      {count}
+      <Link to="/">
+        Back Count:
+        <div id="countdown-number"></div>
+        <svg>
+          <circle r="18" cx="20" cy="20"></circle>
+        </svg>
+      </Link>
+    </div>
+  );
 }
 
 export default GameModule;
