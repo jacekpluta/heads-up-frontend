e918a882599d0dbd7b76d2e36d70ac13cd6f48c3
jacagaca <jacagaca1@o2.pl>
2019-11-11 23:25:47 +0100

    counter start

--- src/components/CounterStart.js
+++ src/components/CounterStart.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import useInterval from "./UseInterval";
 import "../counter.scss";
+import $ from "jquery";
+
+function stop() {
+  $(".demo").addClass("stop");
+}
 
 function CounterStart() {
   const [countStart, setCount] = useState(5);
@@ -8,7 +13,7 @@ function CounterStart() {
   const [isRunning, setIsRunning] = useState(true);
 
   useInterval(
-    props => {
+    () => {
       // Your custom logic here
       setCount(countStart - 1);
     },
@@ -19,6 +24,7 @@ function CounterStart() {
   useEffect(() => {
     if (countStart === 0) {
       setIsRunning(false);
+      stop();
       setCount("Start");
     }
   }, [countStart]);
@@ -26,6 +32,45 @@ function CounterStart() {
   return (
     <>
       <h1>{countStart}</h1>
+      <div class="demo">
+        <div class="demo__colored-blocks">
+          <div class="demo__colored-blocks-rotater">
+            <div class="demo__colored-block"></div>
+            <div class="demo__colored-block"></div>
+            <div class="demo__colored-block"></div>
+          </div>
+          <div class="demo__colored-blocks-inner"></div>
+          <div class="demo__text">Start</div>
+        </div>
+        <div class="demo__inner">
+          <svg class="demo__numbers" viewBox="0 0 100 100">
+            <defs>
+              <path class="demo__num-path-1" d="M40,28 55,22 55,78" />
+              <path
+                class="demo__num-join-1-2"
+                d="M55,78 55,83 a17,17 0 1,0 34,0 a20,10 0 0,0 -20,-10"
+              />
+              <path
+                class="demo__num-path-2"
+                d="M69,73 l-35,0 l30,-30 a16,16 0 0,0 -22.6,-22.6 l-7,7"
+              />
+              <path class="demo__num-join-2-3" d="M28,69 Q25,44 34.4,27.4" />
+              <path
+                class="demo__num-path-3"
+                d="M30,20 60,20 40,50 a18,15 0 1,1 -12,19"
+              />
+            </defs>
+            <path
+              class="demo__numbers-path"
+              d="M-10,20 60,20 40,50 a18,15 0 1,1 -12,19 
+               Q25,44 34.4,27.4
+               l7,-7 a16,16 0 0,1 22.6,22.6 l-30,30 l35,0 L69,73 
+               a20,10 0 0,1 20,10 a17,17 0 0,1 -34,0 L55,83 
+               l0,-61 L40,28"
+            />
+          </svg>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/GameModule.js
+++ src/components/GameModule.js
@@ -2,22 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CounterStart from "./CounterStart";
 import CounterTimer from "./CounterTimer";
+
 function GameModule() {
-  if (countStart === true) {
-  }
   return (
     <div className="GameModule">
-      <CounterStart>{props.countStart}</CounterStart>
+      <CounterStart></CounterStart>
       <CounterTimer></CounterTimer>
       <h1>Game</h1>
 
-      <Link to="/">
-        Back:
-        <div id="countdown-number"></div>
-        <svg>
-          <circle r="18" cx="20" cy="20"></circle>
-        </svg>
-      </Link>
+      <Link to="/">Back:</Link>
     </div>
   );
 }
