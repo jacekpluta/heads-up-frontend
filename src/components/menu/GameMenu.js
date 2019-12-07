191819f6b171c1f13cf8b584d69f50abee9c9bf7
jacagaca <jacagaca1@o2.pl>
2019-12-07 19:16:54 +0100

    no message

--- src/components/menu/GameMenu.js
+++ src/components/menu/GameMenu.js
@@ -5,11 +5,18 @@ import GameTitle from "./GameTitle";
 import Variants from "./Variants";
 
 const pStyle = {
-  fontSize: "36px",
   textAlign: "center",
   color: "black",
   width: "100%",
-  height: "10vh"
+  marginTop: "0%",
+  marginBottom: "3%",
+  fontSize: "120px",
+  color: "#fff",
+  textShadow:
+    "0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #00DEFF, 0 0 70px #00DEFF, 0 0 80px #00DEFF, 0 0 100px #00DEFF, 0 0 150px #00DEFF"
+  // color: "#f8f8ff",
+  // textShadow:
+  //   "0 3px 0 #b2a98f,0 14px 10px rgba(0,0,0,0.15), 0 24px 2px rgba(0,0,0,0.1), 0 34px 30px rgba(0,0,0,0.1)"
 };
 
 export default function GameMenu(props) {
